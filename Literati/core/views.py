ba36930fc4510b7dc7d2ced53ee9742371d3de8f
Cptzeph23 <asekazephaniah@gmail.com>
2026-02-03 15:37:53 +0300

    views

--- Literati/core/views.py
+++ Literati/core/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Service, ContactMessage
 
 def home(request):
-    return render(request, 'core/home.html')
+    services = Service.objects.all()
+
+    if request.method == 'POST':
+        ContactMessage.objects.create(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            message=request.POST['message']
+        )
+        return redirect('home')
+
+    return render(request, 'core/home.html', {
+        'services': services
+    })
