e0d61a8feec51ad38a2b464e0e4947b3fc30f451
Cptzeph23 <asekazephaniah@gmail.com>
2026-02-03 15:36:02 +0300

    models

--- Literati/core/admin.py
+++ Literati/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Service, ContactMessage
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('category', 'title')
+    list_filter = ('category',)
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
--- Literati/core/models.py
+++ Literati/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Service(models.Model):
+    CATEGORY_CHOICES = [
+        ('Graphics Designing', 'Graphics Designing'),
+        ('Editing', 'Editing'),
+        ('Voice Acting', 'Voice Acting'),
+        ('Public Speaking', 'Public Speaking'),
+    ]
+
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.category} - {self.title}"
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+    
