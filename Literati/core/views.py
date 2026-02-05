1ecc19971a43c5d2a3365041b743ce6a4ee3e90b
Cptzeph23 <asekazephaniah@gmail.com>
2026-02-05 20:41:24 +0300

    settings

--- Literati/core/views.py
+++ Literati/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Service, ContactMessage
+from django.core.mail import send_mail
+from django.conf import settings
 
 def home(request):
     services = Service.objects.all()
@@ -27,3 +29,27 @@ def portfolio(request):
     return render(request, 'core/portfolio.html')
 
 
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+
+        # Save to DB
+        ContactMessage.objects.create(
+            name=name,
+            email=email,
+            message=message
+        )
+
+        # Send Email
+        send_mail(
+            subject=f"New Contact Message from {name}",
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+            fail_silently=False,
+        )
+
+        return redirect('home')
